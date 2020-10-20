4bd61532f96101750510f034eb396a7f67fd5ab6
kldavis52 <kyle.linnard.davis@gmail.com>
2020-10-19 21:50:50 -0400

    added SSLMODE manually

--- studiopal/settings.py
+++ studiopal/settings.py
@@ -111,7 +111,7 @@ DATABASES = {
         "PASSWORD": env("DATABASE_PASSWORD"),
         "HOST": env("DATABASE_HOST"),
         "PORT": env("DATABASE_PORT"),
-        "SSLMODE": env("SSLMODE"),
+        "SSLMODE": True,
     }
 }
 
